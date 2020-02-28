1593566aca9a156178c6703fa377f907d68c5705
Frank Lu <franklu@live.ca>
2020-02-27 22:20:24 -0800

    Add roxygen header

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,15 @@
+#' Bind two factors
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' a <- factor(c("character", "hits", "your", "eyeballs"))
+#' b <- factor(c("but", "integer", "where it", "counts"))
+#' fbind(a, b)
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
